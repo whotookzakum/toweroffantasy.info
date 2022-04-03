e49ccf45663375686db6bee0d163541a3d528197
Zakum <mirveo@gmail.com>
2022-04-03 11:42:37 -0700

    Mimics page redesign

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,19 @@
 
 
 jQuery(document).ready(function ($){
-
+    
+    // Popper Popovers
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl);
+    });
+    
+    // Popper Tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+    
     setMimicInfo(meryl);
     
     $(".mimic-button").click(function(){
