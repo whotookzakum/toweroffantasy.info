6250a135a506e6736cfedaa1a5de1f40f5e08ca5
Zakum <mirveo@gmail.com>
2022-07-16 02:59:56 -0700

    deploy test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  basePath: '/toweroffantsy.info',
 }
 
 module.exports = nextConfig
