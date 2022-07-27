c4978642c32efb0d12c9862d881c75196f283539
Afrodiy <81159859+Afrodiy@users.noreply.github.com>
2022-07-27 20:34:03 +0700

    Update Echo.js

--- data/ru-RU/characters/sr/Echo.js
+++ data/ru-RU/characters/sr/Echo.js
@@ -1,5 +1,7 @@
 const echo = {
     name: "Эхо",
+    uri: "echo",
+    imgSrc: "echo.png",
     rarity: "SR",
     chinaOnly: false,
     weapon: {
@@ -15,76 +17,78 @@ const echo = {
 
             star2: `Базовый прирост к здоровью от оружия увеличивается на **10%**.`,
 
-            star3: `При попадании навыком <strong class='text-green'>Лунный охотник** уменьшает АТК поражённых целей на **5%** and увеличивает АТК персонажа на **5%** в течение **15** секунд (эффект складывается до **3** раз при задевании навыком больше одной цели). Сила эффекта удваивается на целях с эффектом Тяжёлых ран.`,
+            star3: `При попадании навыком *Лунный охотник* уменьшает АТК поражённых целей на **5%** и увеличивает АТК персонажа на **5%** в течение **15** секунд (эффект складывается до **3** раз при задевании навыком больше одной цели). Сила эффекта удваивается на целях с эффектом Тяжёлых ран.`,
 
             star4: `Базовый прирост АТК оружия увеличивается на **20%**.`,
 
             star5: `Удваивает продолжительность оглушения навыков уклонения.  
-                <abbr title='China Exclusive' class='cn-flag'>CN</abbr> Удваивает продолжительность оглушения навыков уклонения вперёд.`,
+                <abbr title='China Exclusive'/> Удваивает продолжительность оглушения навыков уклонения вперёд.`,
 
             star6: `Навык высвобождения наносит тем больше урона, чем дальше пролетит, вплоть до максимального бонуса урона в **100%**.`
         },
-        skills: [
-            {
-                name: "Thrust",
-                type: "normal",
-                desc: `While on the ground, swing the spear to attack 5 times in a row.  
-                        First Attack: Deal damage equal to **24.7%** of ATK + **1** and knock the target back a short distance.  
-                        Second Attack: Deal damage equal to **22.6%** of ATK + **1** and knock the target back a short distance.  
-                        Third Attack: Deal damage equal to **47.2%** of ATK + **2** and knock the target back a short distance.  
-                        Fourth Attack: Deal damage equal to **65.2%** of ATK + **3** and knock the target back a short distance.  
-                        Fifth Attack: Deal damage equal to **114.95%** of ATK + **6** and knock the target back a significant distance.`,
-            },
-            {
-                name: "Jumping Strike",
-                type: "normal",
-                desc: `While airborne, tap normal attack to attack 4 times in a row in the air.  
-                        First Attack: Deal damage equal to **27.4%** of ATK + **1**.  
-                        Second Attack: Deal damage equal to **23.2%** of ATK + **1**.  
-                        Third Attack: Deal damage equal to **45.8%** of ATK + **2**.  
-                        Fourth Attack: Deal damage equal to **110%** of ATK + **6**.`,
-            },
-            {
-                name: "Crashing Drive",
-                type: "normal",
-                desc: `Tap and hold normal attack while airborne (or tap when a target is selected), or tap normal attack while climbing, jumping backward, or using the Jetpack to trigger Crashing Drive. While falling, deal damage equal to **8%** of ATK + **1** each hit. Upon landing, deal damage equal to **48.5%** of ATK + **3** and knock the target down. The higher the location is when triggered, the higher the damage dealt, up to **600%** of ATK.`,
-            },
-            {
-                name: "Spear Flurry",
-                type: "normal",
-                desc: `After the third normal attack, hold the normal attack button to trigger Spear Flurry. Strike the target and knock them **into the air**, dealing damage equal to **49.2%** of ATK + **3**. Tap again to continue attacking the airborne target, dealing up to **98%** of ATK + **5** damage to a single target.`,
-            },
-            {
-                name: "Sneak Attack",
-                type: "normal",
-                desc: `Thrust the spear forward, dealing damage equal to **570%** of ATK + **30**.`,
-            },
-            {
-                name: "Flying Kick",
-                type: "dodge",
-                desc: `Tap normal attack during the short perfect dodge window (tap an arrow button before dodging) to trigger Flying Kick. Once hit the target, deal damage equal to **72%** of ATK + **4**, knock the target back significantly, and stun them for **1** second (non-Wanderer targets) or **0.5** seconds (Wanderers).`,
-            },
-            {
-                name: "Lunging Smash",
-                type: "dodge",
-                desc: `Tap normal attack during the short period after dodging to trigger Lunging Smash. Slam the long spear into the ground, dealing damage equal to **52.8%** of ATK + **3** and knocking targets **into the air**.`,
-            },
-            {
-                name: "Dodge",
-                type: "dodge",
-                desc: `Dodge right before getting hit to trigger a Phantasia, reducing the speed of enemies within an area. Become immune to hitstun for **0.5** seconds while dodging.`,
-            },
-            {
-                name: "Лунный охотник",
-                type: "skill",
-                desc: `Launch the target into the air and go airborne, dealing damage equal to **127.5%** of ATK + **7**. **25**-second cooldown.`,
-            },
-            {
-                name: "Thunderclap",
-                type: "discharge",
-                desc: `When a <b>weapon is fully charged</b> or triggers <b>Phantasia</b>, switching to this weapon from another weapon **removes all debuffs from the wielder**, who jumps in the air and throws a lightning spear that causes area explosion to deal **296.7%** x ATK + **16** damage plus **paralyzing** the target for **1** second.`,
-            }
-        ],
+        abilities: {
+            normal: [
+                {
+                    name: "Напор",
+                    description: `Находясь на земле, нажимайте кнопку удара, чтобы совершить серию до 5 атак.`,
+                    breakdown: [
+                        'Наносит **24,7%** от АТК + **1** урона и несильно отталкивает цель.',
+                        'Наносит **22,6%** от АТК + **1** урона и несильно отталкивает цель.',
+                        'Наносит **47,2%** от АТК + **2** урона и несильно отталкивает цель.',
+                        'Наносит **65,%** от АТК + **3** урона и несильно отталкивает цель.',
+                        'Наносит **114,95%** от АТК + **6** урона и отталкивает цель.'
+                    ]
+                },
+                {
+                    name: "Удар в прыжке",
+                    description: `Находясь в воздухе, нажимайте кнопку удара, чтобы совершить серию до 4 атак.`,
+                    breakdown: [
+                        'Наносит **27,4%** от АТК + **1** урона.',
+                        'Наносит **23,2%** от АТК + **1** урона.',
+                        'Наносит **45,%** от АТК + **2** урона.',
+                        'Наносит **110%** от АТК + **6** урона.'
+                    ]
+                },
+                {
+                    name: "Атака в падении",
+                    description: `Находясь в воздухе, нажмите и удерживайте кнопку удара или просто нажмите её во время карабкания, прыжка назад или планирования на Джетпаке, чтобы применить атаку в падении, каждым ударом нанося **8%** от АТК + **1** урона. По приземлении наносит урон, равный **48,5%** от ATK + **3**, и сбивает цель с ног. Чем выше персонаж при применении атаки в падении, тем выше будет наносимый урон, вплоть до максимального в **600%** от АТК.`,
+                },
+                {
+                    name: "Шквал копий",
+                    description: `Нажмите и удерживайте кнопку удара после третьей совершённой базовой атаки, чтобы **подбросить цель в воздух**, нанося **49,2%** от АТК + **3** урона. Нажмите снова, чтобы продолжить атаковать цель в воздухе, нанося **98%** от АТК + **5** урона.`,
+                },
+                {
+                    name: "Скрытная атака",
+                    description: `Пригнувшись, подойдите к противнику сзади и нажмите кнопку удара, чтобы нанести **570%** от АТК + **30** урона.`,
+                }
+            ],
+            dodge: [
+                {
+                    name: "Летящий пинок",
+                    description: `Нажмите кнопку удара в течение короткого периода после уклонения в сторону, чтобы нанести **72%** от АТК + **4** урона, отбросить цель и **оглушить** её на 1 секунду (против обычных врагов) или 0,5 секунд (против других Странников).`,
+                },
+                {
+                    name: "Стремительный удар",
+                    description: `Нажмите кнопку удара в течение короткого периода после уклонения, чтобы нанести **52,8%** от АТК + **3** урона и **подбросить врагов в воздух**.`,
+                },
+                {
+                    name: "Уклонение",
+                    description: `Уклонение прямо перед получением удара создаёт зону ***Фантазии***, которая уменьшает скорость врагов. Перезарядка: **15** секунд. При уклонении персонаж получает неуязвимость к эффектам оглушения в течение 0,5 секунд.`,
+                }
+            ],
+            skill: [
+                {
+                    name: "Лунный охотник",
+                    description: `Подбрасывает цель и себя в воздух, нанося **127,5%** от АТК + **7** урона. Перезарядка: **25** секунд.`,
+                }
+            ],
+            discharge: [
+                {
+                    name: "Раскат грома",
+                    description: `Когда ***оружие полностью заряжено***, при смене оружия на это **сбрасывает все негативные эффекты с персонажа**, который взмывает в воздух и бросает в цель громовое копьё, нанося **296,7%** от АТК + **16** урона по площади, также **парализуя** цель на 1 секунду.`,
+                }
+            ]
+        },
         recommendedMatrix: {
             set2: ["samir", "king", "shiro"],
             set3: ["sobek", "offensivelogic"]
@@ -115,7 +119,7 @@ const echo = {
         height: '162 см',
         birthplace: 'Уоррен',
         horoscope: 'лев',
-        birthday: '14 Августа',
+        birthday: '14 Авг',
         voiceActors: {
             jp: "大久保瑠美 (Руми Окубо)",
             en: "Jeannie Tirado (Джинни Тирадо)",
