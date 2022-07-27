6fd00382c1fd13cb704d83b7d740d99ce0290d13
Afrodiy <81159859+Afrodiy@users.noreply.github.com>
2022-07-27 15:48:43 +0700

    Add files via upload

---
+++ data/ru-RU/relics/relicList.js
@@ -0,0 +1,49 @@
+
+import couant from "./sr/Couant"
+import cyberneticarm from "./sr/CyberneticArm"
+import hoveringcannon from "./sr/HoveringCannon"
+import jetboard from "./sr/Jetboard"
+import jetpack from "./sr/Jetpack"
+import lavabomb from "./sr/LavaBomb"
+import magneticpulse from "./sr/MagneticPulse"
+import magneticstorm from "./sr/MagneticStorm"
+import missilebarrage from "./sr/MissileBarrage"
+import omniumhandcannon from "./sr/OmniumHandCannon"
+import quantumcloak from "./sr/QuantumCloak"
+import strangecube from "./sr/StrangeCube"
+import alternatedestiny from "./ssr/AlternateDestiny"
+import colossusarms from "./ssr/ColossusArms"
+import confinement from "./ssr/Confinement"
+import couant2 from "./ssr/Couant2"
+import drone from "./ssr/Drone"
+import hologramprojector from "./ssr/HologramProjector"
+import hoverboard from "./ssr/Hoverboard"
+import omniumshield from "./ssr/OmniumShield"
+import spacetimerift from "./ssr/SpacetimeRift"
+import typevarmor from "./ssr/TypeVArmor"
+
+
+export const RELICS = [
+    hoverboard,
+    couant2,
+    colossusarms,
+    omniumshield,
+    typevarmor,
+    spacetimerift,
+    confinement,
+    drone,
+    hologramprojector,
+    alternatedestiny,
+    jetpack,
+    quantumcloak,
+    hoveringcannon,
+    jetboard,
+    magneticstorm,
+    missilebarrage,
+    couant,
+    strangecube,
+    omniumhandcannon,
+    lavabomb,
+    cyberneticarm,
+    magneticpulse
+]
