7142f33982a5d211277f73454f2abe967c7b4787
Afrodiy <81159859+Afrodiy@users.noreply.github.com>
2022-07-27 14:38:02 +0700

    Update aidaknight.js

--- data/ru-RU/mounts/aidaknight.js
+++ data/ru-RU/mounts/aidaknight.js
@@ -1,11 +1,11 @@
 const aidaknight = {
-    name: "Aida Knight",
+    name: "Рыцарь Аиды",
     uri: "aida-knight",
     imgSrc: "aidaknight.png",
     chinaOnly: true,
     parts: {
         part1: {
-            source: "Rewarded for purchasing the 4th Premium Battle Pass.",
+            source: "Награда за покупку 4-го Премиального Боевого пропуска.",
         }
     }
 }
