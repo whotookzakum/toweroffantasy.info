3dfd5857b5fd1d226e6a0d1de0e6d5b63b90286a
Afrodiy <81159859+Afrodiy@users.noreply.github.com>
2022-07-27 15:42:36 +0700

    Update mountList.js

--- data/ru-RU/mounts/mountList.js
+++ data/ru-RU/mounts/mountList.js
@@ -4,6 +4,7 @@ import chaser from "./chaser";
 import crimsonmeteor from "./crimsonmeteor";
 import desertrower from "./desertrower";
 import dustwheeler from "./dustwheeler";
+import futurechariot from "./futurechariot";
 import heavybike from "./heavybike";
 import jerboa from "./jerboa";
 import jerboapaincar from "./jerboapaincar";
@@ -13,6 +14,7 @@ import omniumbeastvii from "./omniumbeastvii";
 import voyager from "./voyager";
 
 export const MOUNTS = [
+    futurechariot,
     jerboapaincar,
     desertrower,
     jerboa,
