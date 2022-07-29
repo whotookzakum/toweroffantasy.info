f03a23e432579ede86763a282cd9c6e16f6dc348
Afrodiy <81159859+Afrodiy@users.noreply.github.com>
2022-07-29 21:02:39 +0700

    Update Ene.js

--- data/ru-RU/characters/sr/Ene.js
+++ data/ru-RU/characters/sr/Ene.js
@@ -1,5 +1,7 @@
 const ene = {
     name: "Эне",
+    uri: "ene",
+    imgSrc: "ene.png",
     rarity: "SR",
     chinaOnly: false,
     weapon: {
@@ -11,86 +13,101 @@ const ene = {
         charge: ["B", "6.00"],
         materials: ["ice", "green", "black"],
         advancement: {
-            star1: "Провоцирует ближайших врагов при смене на это оружие, поглощая **1** следующий удар в течение **8** секунд и получая эффект Гипертела на **8** секунд. Перезарядка: **15** секунд.",
+            star1: "Провоцирует ближайших врагов при смене на это оружие, поглощая **1** следующий удар в течение 8 секунд и получая эффект **Гипертела** на 8 секунд. Перезарядка: **15** секунд.",
             
             star2: "Базовый прирост к здоровью от оружия увеличивается на **10%**.",
            
-            star3: "После получения урона весь получаемый урон уменьшается на **50%** в течение следующих **6** секунд. Перезарядка: **20** секунд.",
+            star3: "После получения урона весь получаемый урон уменьшается на **50%** в течение следующих 6 секунд. Перезарядка: **20** секунд.",
             
             star4: "Базовый прирост к здоровью от оружия увеличивается на **20%**.",
            
-            star5: "Базовые атаки **оглушают** цель на **2,5** секунды. Перезарядка: **20** секунд.",
+            star5: "Базовые атаки оглушают цель на 2,5 секунды. Перезарядка: **20** секунд.",
            
-            star6: "Навык высвобождения наносит на **10%** больше урона и увеличивает Урон Холодом на **10%** в течение **25** секунд."
+            star6: "Навык высвобождения наносит на **10%** больше урона и увеличивает Урон Холодом на **10%** в течение 25 секунд."
         },
-        skills: [
-            {
-                name: "Normal Attack",
-                type: "normal",
-                desc: `While on the ground, swing the hammer to attack 4 times in a row.  
-                        First Attack: Deal damage equal to **76.3%** of ATK + **4** and knock the target back a short distance.  
-                        Second Attack: Deal damage equal to **62.1%** of ATK + **3** and knock the target back a short distance.  
-                        Third Attack: Deal damage equal to **157.4%** of ATK + **8** and knock the target back a short distance.  
-                        Fourth Attack: Deal damage equal to **263.8%** of ATK + **14** and launch the target.  
-                        While Outburst is active, swing the hammer to attack 4 times in a row.  
-                        First Attack: Deal damage equal to **75.5%** of ATK + **4** and knock the target back a short distance.  
-                        Second Attack: Deal damage equal to **65%** of ATK + **3** and knock the ed target back a short distance.  
-                        Third Attack: Deal damage equal to **171.3%** of ATK + **9** and knock the target back a short distance.  
-                        Fourth Attack: Deal damage equal to **315.3%** of ATK + **17** and knock the target **into the air**.`,
-            },
-            {
-                name: "Pummel",
-                type: "normal",
-                desc: `While airborne or after jumping once, tap normal attack to trigger Jumping Blast. Deal damage equal to **121.7%** of ATK + **6** and knock the target into the air.  
-                        While Outburst is active, deal damage equal to **133.9%** of ATK + **7** and knock the target **into the air**. Deal greater damage depending on the user's altitude when the ability is triggered, up to **600%** of ATK.`,
-            },
-            {
-                name: "Shockwave",
-                type: "normal",
-                desc: `Tapping and holding while airborne, Double-jumping, short tapping in the air when there are targets nearby, climbing, jumping backward, or attacking while using the Jetpack will trigger a Shockwave. Knock the target **into the air 4** times, dealing damage equal to **34.8%** of ATK + **2** each time. Cost **250** endurance.  While Outburst is active, Shockwave knocks the target **into the air 5** times, dealing damage equal to **38.3%** of ATK + **2** each time. Deal greater damage depending on the user's altitude when the ability is triggered, up to **600%** of ATK.`,
-            },
-            {
-                name: "Sneak Attack",
-                type: "normal",
-                desc: `Approach the enemy from behind while crouching, then tap normal attack to use Sneak Attack, dealing damage equal to **427.5%** of ATK + **23**.`,
-            },
-            {
-                name: "Focus Smash",
-                type: "normal",
-                desc: `Tap and hold normal attack to trigger Focus Smash (can be charged up to 3 stacks). Grant immunity to all control effects and hyperbody while charging.  
-                        First Stack: Deal damage equal to **110%** of ATK + **6** and knock the target into the air.  
-                        Second Stack: Deal damage equal to **276%** of ATK + **15** and knock the target **into the air**.  
-                        Third Stack: Deal damage equal to **518%** of ATK + **27** and knock the target **into the air**.  
-                        While Outburst is active, tap and hold normal attack to trigger Furious Strikes, dealing damage equal to **501.6%** of ATK + **26** and knocking the target **into the air**.`,
-            },
-            {
-                name: "Jumping Smash",
-                type: "dodge",
-                desc: `Use the arrow buttons and the dodge button to trigger Jumping Smash, unleashing a Shockwave that knocks the enemy into the air **4** times, dealing damage equal to **161.6%** of ATK + **9**.`,
-            },
-            {
-                name: "Worldsplitter",
-                type: "dodge",
-                desc: `Tap normal attack during the short period of time after a dodge to trigger Worldsplitter, unleashing **3** shockwaves forward, each dealing damage equal to **52.4%** of ATK + **3**, and knocking enemies into the air.  
-                        While in Outburst mode, fire **5** shockwaves forward, each dealing damage equal to **52.4%** of ATK + **3** and knocking the target into the air.`,
-            },
-            {
-                name: "Dodge",
-                type: "dodge",
-                desc: `Dodge right before getting hit to activate a Phantasia, which reduces the speed of enemies within the area. Cooldown: 15 seconds. While dodging, you gain hitstun immunity for **0.5** seconds.`,
-            },
-            {
-                name: "Outburst",
-                type: "skill",
-                desc: `Switch to activate Outburst, increasing damage dealt for **25** seconds. While Outburst is active, skills trigger Windfury, dealing damage equal to **219%** of ATK + **12**, knock the target **into the air**, and create an air current on the ground. While standing in the current, dodging has no cost. Cooldown: **45** seconds (Windfury's cooldown: **10** seconds).`,
-            },
-            {
-                name: "Overwhelming Maul",
-                type: "discharge",
-                desc: `When <b>weapon charge is full</b> or <b>Phantasia</b> is triggered, **remove all debuffs from the wielder** and knock nearby targets **into the air** upon switching to this weapon, dealing damage equal to **153.3%** of ATK + **8**. Also summon a hammer to fall on nearby targets, dealing damage equal to **153.3%** of ATK + **8**.`,
-            }
-        ],
-        recChips: {
+        abilities: {
+            normal: [
+                {
+                    name: "Обычная атака",
+                    input: ["Атака x4"],
+                    description: `Находясь на земле, нажимайте кнопку удара, чтобы совершить серию до 4 атак.`,
+                    breakdown: [
+                        `Наносит **76,3%** от АТК + **4** урона и несильно отталкивает цель.  
+                        При активной Эманации льда наносит **75.5%** от АТК + **4** урона и несильно отталкивает цель.`,
+                        `Наносит **62.1%** от АТК + **3** урона и несильно отталкивает цель.  
+                        При активной Эманации льда наносит **65%** от АТК + **3** урона и несильно отталкивает цель.`,
+                        `Наносит **157.4%** от АТК + **8** урона и несильно отталкивает цель.  
+                        При активной Эманации льда наносит **171.3%** от АТК + **9** урона и несильно отталкивает цель.`,
+                        `Наносит **263.8%** от АТК + **14** урона и сбивает цель.  
+                        При активной Эманации льда наносит **315.3%** от АТК + **17** урона и **подбрасывает цель в воздух**.`
+                    ]
+                },
+                {
+                    name: "Избивание",
+                    input: ["Прыжок","Атака"],
+                    description: `Находясь в воздухе или после прыжка, нажмите кнопку атаки, чтобы нанести **121,7%** от АТК + **6** урона и **подбросить цель в воздух**.  
+                            При активной Эманации льда наносит **133,9%** от АТК + **7** урона и **подбрасывает цель в воздух**. Чем выше персонаж при применении атаки, тем выше будет наносимый урон, вплоть до максимального в **600%** от АТК.`,
+                },
+                {
+                    name: "Ударная волна",
+                    input: ["Прыжок","Атака","Удерживание"],
+                    description: 
+                        `Находясь в воздухе, нажмите и удерживайте кнопку удара или просто нажмите её, когда выбрана цель, во время карабкания, двойного прыжка или планирования на Джетпаке, чтобы применить атаку в падении. Совершает 4 удара, каждый из которых наносит **34,8%** от АТК + **2** урона и **подбрасывает цель в воздух**. Тратит **250** выносливости.  
+                        &nbsp;  
+                        ПРи активной Эманации льда Ударная волна наносит 5 ударов, каждым из которых наносит **38,3%** от АТК + **2** урона и **подбрасывает цель в воздух**. Чем выше персонаж при применении атаки, тем выше будет наносимый урон, вплоть до максимального в **600%** от АТК.`,
+                },
+                {
+                    name: "Скрытная атака",
+                    input: ["Пригнуться","Атака"],
+                    description: `Пригнувшись, подойдите к противнику сзади и нажмите кнопку удара, чтобы нанести **427,5%** от АТК + **23** урона.`,
+                },
+                {
+                    name: "Концентрированный удар",
+                    input: ["Атака", "Удерживание"],
+                    description: 
+                        `Нажмите и удерживайте кнопку удара, чтобы зарядить Концентрированный удар до 3 стадий. Во время заряда персонаж получает **Гипертело** и иммунитет к эффектам контроля.  
+                        &nbsp;  
+                        При активной Эманации льда нажмите и удерживайте кнопку удара, чтобы совершать Яростные удары, суммарно нанося **501,6%** от АТК + **26** урона и **подбрасывая цель в воздух**.`,
+                    breakdown: [
+                        'Первая стадия: Наносит **110%** от АТК + **6** урона и **подбрасывает цель в воздух**.',
+                        'Вторая стадия: Наносит **276%** от АТК + **15** урона и **подбрасывает цель в воздух**.',
+                        'Третья стадия: Наносит **518%** от ATK + **27** урона и **подбрасывает цель в воздух**.'
+                    ]
+                }
+            ],
+            dodge: [
+                {
+                    name: "Удар в прыжке",
+                    input: ["Кнопки движения","Уклонение","Атака"],
+                    description: `Нажмите кнопку удара в течение короткого периода после уклонения в сторону, чтобы образовать ударную волну, которая 4 раза наносит **161,6%** от АТК + **9** урона и **подбрасывает цель в воздух**.`,
+                },
+                {
+                    name: "Разрушитель миров",
+                    input: ["Уклонение","Атака"],
+                    description: 
+                        `Нажмите кнопку удара в течение короткого периода после уклонения, чтобы выпустить вперёд **3** ударные волны, каждая из которых наносит **52,4%** от АТК + **3** урона и **подбрасывает цель в воздух**.  
+                        &nbsp;  
+                        При активной Эманации льда выпускает вперёд **5** ударных волн, каждая из которых наносит **52,4%** от АТК + **3** урона и **подбрасывает цель в воздух**`,
+                },
+                {
+                    name: "Уклонение",
+                    description: `Уклонение прямо перед получением удара создаёт зону ***Фантазии***, которая уменьшает скорость врагов. Перезарядка: **15** секунд. При уклонении персонаж получает неуязвимость к эффектам оглушения в течение **0,5** секунд.`,
+                }
+            ],
+            skill: [
+                {
+                    name: "Эманация льда",
+                    description: `Активирует Эманацию льда, увеличивая наносимый урон на **25** секунд. При активной Эманации льда базовые атаки применяют Ярость ветра, нанося **219%** от АТК + **12** урона и **подбрасывая цель в воздух**, также создавая воздушный поток. Персонаж не тратит доступное количество уклонений, пока находится в воздушном потоке. Перезарядка: **45** секунд (Перезарядка Ярости ветра: **10** секунд).`,
+                }
+            ],
+            discharge: [
+                {
+                    name: "Ошеломляюшая кувалда",
+                    description: `Когда ***оружие полностью заряжено***, при смене оружия на это **сбрасывает все негативные эффекты с персонажа** и **подбрасывает ближайших врагов в воздух**, нанося **153,3%** от АТК + **8** урона. Также призывает над каждой целью призывает молот, который падает на неё и наносит **153,3%** от АТК + **8** урона.`,
+                }
+            ]
+        },
+        recommendedMatrix: {
             set2: ["king", "shiro"],
             set3: ["sobek", "offensivelogic"]
         }
