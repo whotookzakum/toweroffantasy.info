189470e121ccec18e83e33b3b2d6a78e76fe8509
Zakum <mirveo@gmail.com>
2022-08-01 10:47:44 -0700

    Exploration data

---
+++ data/en-US/exploration/exploration.js
@@ -0,0 +1,350 @@
+const exploration = [
+    // Aesperia
+    // ruins
+    // training facility
+    {
+        name: "Dandelion",
+        uri: "dandelion",
+        imgSrc: "dandelion.png",
+        type: "basic",
+        solution: "Use any weapon to hit the dandelion and then hit the seed that has the black nucleus in it.",
+        videoSrc: ""
+    },
+    {
+        name: "Tar Pit",
+        uri: "tar-pit",
+        imgSrc: "tarpit.png",
+        type: "elemental",
+        solution: "Attack with a Flame-element weapon.",
+        videoSrc: ""
+    },
+    {
+        name: "Kerosenia",
+        uri: "kerosenia",
+        imgSrc: "kerosenia.png",
+        type: "elemenal",
+        solution: "Attack with a Flame-element weapon.",
+        videoSrc: ""
+    },
+    {
+        name: "Chowchow",
+        uri: "chowchow",
+        imgSrc: "chowchow.png",
+        type: "basic",
+        solution: "Throw an Ice-Shell Ball into the Chowchow. Ice-Shell Balls are usually found near the Chowchow, though some may be hidden.",
+        videoSrc: ""
+    },
+    {
+        name: "Thornvine",
+        uri: "thornvine",
+        imgSrc: "thornvine.png",
+        type: "elemental",
+        solution: "Attack with a Flame-element weapon.",
+        videoSrc: ""
+    },
+    {
+        name: "Glowshroom",
+        uri: "glowshroom",
+        imgSrc: "glowshroom.png",
+        type: "basic",
+        solution: "Jump on the glowshroom caps in the correct order. If you jumped on the correct one, there will be subtle visual and audio feedback.",
+        videoSrc: ""
+    },
+    {
+        name: "Ring of Echoes",
+        uri: "ring-of-echoes",
+        imgSrc: "ringofechoes.png",
+        type: "basic",
+        solution: "Face the puzzle from the circular pad with a square in the middle, and to the right is a pad between 2 Cactus Plants. Jump on that pad and once you do another Music Pad will light up and just follow the order that the pads glow in.",
+        videoSrc: ""
+    },
+    {
+        name: "Destroyable Rock",
+        uri: "destroyable-rock",
+        imgSrc: "destroyablerock.png",
+        type: "relic",
+        solution: "Use the relic Missile Barrage to break the wall.",
+        videoSrc: ""
+    },
+    {
+        name: "Singularity Rock",
+        uri: "singularity-rock",
+        imgSrc: "singularityrock.png",
+        type: "relic",
+        solution: "Use the relic Strange Cube to push the ball off the statue.",
+        videoSrc: ""
+    },
+    {
+        name: "Primordial Pillar Induction Stone Plate",
+        uri: "",
+        imgSrc: "",
+        type: "relic",
+        solution: "Use the relic Omnium Handcannon on the induction plate.",
+        videoSrc: ""
+    },
+    {
+        name: "Force Sensing Stone Plate",
+        uri: "",
+        imgSrc: "",
+        type: "basic",
+        solution: "Perform an aerial down smash attack on the plate with any weapon.",
+        videoSrc: ""
+    },
+    {
+        name: "Mushroom Man",
+        uri: "mushroom-man",
+        imgSrc: "mushroomman.png",
+        type: "basic",
+        solution: "Speak with the Mushroom Man to participate in a minigame. Complete the game within the time limit.",
+        videoSrc: ""
+    },
+    {
+        name: "AI Servant",
+        uri: "ai-servant",
+        imgSrc: "aiservant.png",
+        type: "basic",
+        solution: "Follow the AI Servant and it will lead you to an NPC.",
+        videoSrc: ""
+    },
+    {
+        name: "Energy Screen",
+        uri: "energy-screen",
+        imgSrc: "energyscreen.png",
+        type: "basic",
+        solution: "Enter the correct passcode into the terminal to remove the barrier.",
+        videoSrc: ""
+    },
+    {
+        name: "Lava Pit",
+        uri: "lava-pit",
+        imgSrc: "lavapit.png",
+        type: "elemental",
+        solution: "Attack with an Ice-element weapon.",
+        videoSrc: ""
+    },
+    {
+        name: "Telescope",
+        uri: "telescope",
+        imgSrc: "telescope.png",
+        type: "basic",
+        solution: "Connect the stars in the shape of the constellation. Correct connections will persist even after pressing reset, so you can continuously attempt all lines and reset until it's solved.",
+        videoSrc: ""
+    },
+    {
+        name: "Abandoned Truck",
+        uri: "abandoned-truck",
+        imgSrc: "abandonedtruck.png",
+        type: "basic",
+        solution: "Enter the correct passcode into the terminal to unlock the truck.",
+        videoSrc: ""
+    },
+    {
+        name: "Concentration Ray",
+        uri: "concentration-ray",
+        imgSrc: "concentrationray.png",
+        type: "basic",
+        solution: "Interact with the concentration rays in order to transmit energy between them. Each concentration ray faces the next ray, so after finding one you can trace them back to the start. There are two of these puzzles in Crown.",
+        videoSrc: ""
+    },
+    {
+        name: "Spliced Metal Box",
+        uri: "spliced-metal-box",
+        imgSrc: "splicedmetalbox.png",
+        type: "basic",
+        solution: "Push the the box pieces together to connect the pattern on them.",
+        videoSrc: ""
+    },
+    {
+        name: "Earthphyte",
+        uri: "earthphyte",
+        imgSrc: "earthphyte.png",
+        type: "basic",
+        solution: "Throw the matching color flower bud into the Earthphyte.",
+        videoSrc: ""
+    },
+    {
+        name: "Nucleus Drone",
+        uri: "nucleus-drone",
+        imgSrc: "nucleusdrone.png",
+        type: "basic",
+        solution: "Destroy the drone with any weapon. Ranged weapon recommended.",
+        videoSrc: ""
+    },
+    {
+        name: "Silvercap Tree",
+        uri: "silvercap-tree",
+        imgSrc: "silvercaptree.png",
+        type: "basic",
+        solution: "Interact with the tree to begin a minigame. Drop the crystal into the hole at the bottom, moving it left and right to avoid obstacles along the way.",
+        videoSrc: ""
+    },
+
+    // Artificial Island
+    // how to unlock outer islands..
+    // {
+    //     name: "Space Leap",
+    //     chinaOnly: true,
+    //     uri: "space-leap",
+    //     imgSrc: "spaceleap.png",
+    //     type: "basic",
+    //     solution: "Enter one of the portals to teleport to the corresponding endpoint.",
+    //     videoSrc: ""
+    // },
+
+    // Vera Desert
+    {
+        name: "Hungry Critter",
+        chinaOnly: true,
+        uri: "hungry-critter",
+        imgSrc: "hungrycritter.png",
+        type: "basic",
+        solution: "Give the critter the food that it wants.",
+        videoSrc: ""
+    },
+    {
+        name: "Friendly Critter",
+        chinaOnly: true,
+        uri: "friendly-critter",
+        imgSrc: "friendlycritter.png",
+        type: "basic",
+        solution: "Perform the correct action to the critter. Actions can be performed from opening the chat and clicking the emotes button near the Send button. The dancing critter wants your applause and the waving critter wants you to wave!",
+        videoSrc: ""
+    },
+    {
+        name: "Elemental Supply Pod",
+        chinaOnly: true,
+        uri: "elemental-supply-pod",
+        imgSrc: "elementalsupplypod.png",
+        type: "elemental",
+        solution: "Attack 4 nearby floating orbs with the correct element to unseal the supply pod.",
+        videoSrc: ""
+    },
+    {
+        name: "Trace Stone",
+        chinaOnly: true,
+        uri: "trace-stone",
+        imgSrc: "tracestone.png",
+        type: "basic",
+        solution: "Stand in an area where the two split stones line up and the sigil glows yellow. Shift your camera to get the exact position that the stones line up.",
+        videoSrc: ""
+    },
+    {
+        name: "Lost Gerbil",
+        chinaOnly: true,
+        uri: "lost-gerbil",
+        imgSrc: "lostgerbil.png",
+        type: "basic",
+        solution: "Guide the gerbil back to its burrow. Follow the route of small desert plants buried in the sand.",
+        videoSrc: ""
+    },
+    {
+        name: "Gerbil Nest",
+        chinaOnly: true,
+        uri: "gerbil-nest",
+        imgSrc: "gerbilnest.png",
+        type: "basic",
+        solution: "Interact with the glittering gerbil to play a game. The 3 gerbils will swap burrows repeatedly. Find the glittering gerbil once they have finished swapping.",
+        videoSrc: ""
+    },
+    {
+        name: "Watcher",
+        chinaOnly: true,
+        uri: "watcher",
+        imgSrc: "watcher.png",
+        type: "basic",
+        solution: "Interact with the watcher before it closes its shell. Watchers will close their shell when you approach them. You can dash and rush straight at them, use the Quantum Cloak, or approach from above with the Jetpack.",
+        videoSrc: ""
+    },
+    {
+        name: "Cactus Fruit",
+        chinaOnly: true,
+        uri: "cactus-fruit",
+        imgSrc: "cactusfruit.png",
+        type: "basic",
+        solution: "Attack the cactus with any weapon, then attack the bud that rises up afterwards.",
+        videoSrc: ""
+    },
+    {
+        name: "Cave Mushroom",
+        chinaOnly: true,
+        uri: "cave-mushroom",
+        imgSrc: "cavemushroom.png",
+        type: "",
+        solution: "",
+        videoSrc: ""
+    },
+    {
+        name: "",
+        chinaOnly: true,
+        uri: "",
+        imgSrc: "",
+        type: "",
+        solution: "",
+        videoSrc: ""
+    },
+
+    // Mirroria
+    // daily minigames
+    // gossip
+
+    // Dark & Gray Zones
+    // dark zone puzzles
+    // gray zone enemy portals that give exploration %
+
+    // Disorderly Abyss
+    // ore collection
+    {
+        name: "Energy Repeater",
+        chinaOnly: true,
+        uri: "energy-repeater",
+        imgSrc: "energyrepeater.png",
+        type: "basic",
+        solution: "Collect energy from the small pillar with an upside down pyramid (energy will appear as a glowing aura around your character's wrist). Find and strike the energy-sealed box before the aura runs out.",
+        videoSrc: ""
+    },
+    {
+        name: "Sequence Monitor",
+        chinaOnly: true,
+        uri: "sequence-monitor",
+        imgSrc: "sequencemonitor.png",
+        type: "basic",
+        solution: "Interact with the monitor to view its camera. Go to the exact spot indicated by the camera.",
+        videoSrc: ""
+    },
+    {
+        name: "Chaos Cube",
+        chinaOnly: true,
+        uri: "chaos-cube",
+        imgSrc: "chaoscube.png",
+        type: "basic",
+        solution: "Attack the cube until it breaks.",
+        videoSrc: ""
+    },
+    {
+        name: "Simulation Engine",
+        chinaOnly: true,
+        uri: "simulation-engine",
+        imgSrc: "simulationengine.png",
+        type: "basic",
+        solution: "Interact to begin a simulated battle. Defeat all enemies before the simulation ends.",
+        videoSrc: ""
+    },
+    {
+        name: "Energy Recovery",
+        chinaOnly: true,
+        uri: "energy-recovery",
+        imgSrc: "energyrecovery.png",
+        type: "basic",
+        solution: "Interact to scatter 2~4 energy crystals nearby. Collect them all within the time limit.",
+        videoSrc: ""
+    },
+    {
+        name: "Hidden Passage",
+        chinaOnly: true,
+        uri: "hidden-passage",
+        imgSrc: "hiddenpassage.png",
+        type: "basic",
+        solution: "Interact to generate platforms for an alternative path.",
+        videoSrc: ""
+    },
+]
