3adfe84cb4ae31295140f2ce6b4134d2a810d984
Zakum <mirveo@gmail.com>
2022-09-01 05:36:54 -0700

    create windblade

---
+++ data/en-US/matrices/r/WindBlade.js
@@ -0,0 +1,10 @@
+export const windBlade = {
+    name:"Wind Blade",
+    uri: "wind-blade",
+    imgSrc: "windblade.webp",
+    rarity:"R",
+    chinaOnly: false,
+    matrix: { 
+        set3: "Increase shatter capability by **18%**."
+    }
+}
