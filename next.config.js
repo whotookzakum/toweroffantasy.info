088d0e85d2f58ef01a72a17786306fe4c418c027
Zakum <mirveo@gmail.com>
2022-09-08 04:13:08 -0700

    custom headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/*',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public',
+          },
+          {
+            key: 'cache-control',
+            value: 'max-age=604800',
+          },
+        ],
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
