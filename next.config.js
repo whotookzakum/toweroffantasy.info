7f6d97e96dd7c9967b32c91ff3aca23c84261ac9
Zakum <mirveo@gmail.com>
2022-09-08 04:14:24 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/*',
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'public',
-          },
-          {
-            key: 'cache-control',
-            value: 'max-age=604800',
-          },
-        ],
-      },
-    ]
-  },
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
