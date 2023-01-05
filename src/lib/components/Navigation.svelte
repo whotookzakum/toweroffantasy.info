2150440a74504d3402174b9a604a1203d1bcccae
Zakum <mirveo@gmail.com>
2023-01-05 03:26:29 -0800

    layout perfected

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -65,16 +65,17 @@
     ];
 </script>
 
-
-<button class="nav-toggle" on:click={() => (navIsOpen = !navIsOpen)}>
-    <Icon
-        icon={navIsOpen ? "ph:x" : "mdi:menu"}
-        width="32"
-        height="32"
-        color="var(--accent)"
-    />
-</button>
-<nav class:open={navIsOpen}>
+<nav>
+    <div class="toggle-wrapper">
+        <button class="nav-toggle" on:click={() => (navIsOpen = !navIsOpen)}>
+            <Icon
+                icon={navIsOpen ? "ph:x" : "mdi:menu"}
+                width="32"
+                height="32"
+                color="var(--accent)"
+            />
+        </button>
+    </div>
     <a class="home-link" href="/">
         <img
             src="/images/mia.png"
@@ -126,33 +127,23 @@
 <style lang="scss">
     nav {
         background: var(--surface1);
-        // box-shadow: 0 0 4px 2px var(--surface-shadow);
         width: 250px;
         height: 100vh;
         overflow-y: auto;
-        position: fixed;
-        z-index: 9000;
-        margin-left: 0;
+        position: sticky;
         top: 0;
+        z-index: 9000;
         box-sizing: border-box;
         display: flex;
         flex-direction: column;
         --padding: 0.75rem 1.5rem;
-
-        &::-webkit-scrollbar {
-            display: none;
-        }
-        -ms-overflow-style: none;
-        scrollbar-width: none;
-        transition: all 0.3s ease;
     }
 
     .nav-toggle {
-        display: none;
         position: fixed;
-        z-index: 9000;
         top: 1rem;
         right: 1rem;
+        z-index: 9001;
         background: var(--surface3);
         box-shadow: 0 2px 4px var(--surface-shadow);
         border: 1px solid var(--accent);
@@ -162,12 +153,10 @@
         height: 44px;
     }
 
-    @media (max-width: 800px) {
-        nav:not(.open) {
-            transform: translateX(-250px); // or margin-left
-        }
+    // content-width
+    @media (min-width: 800px) {
         .nav-toggle {
-            display: unset;
+            display: none;
         }
     }
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,40 +5,56 @@
     let navIsOpen;
 </script>
 
-<Navigation bind:navIsOpen />
-<div class="page-wrapper" class:slide={navIsOpen}>
-    <main class="page-contents">
+<div class="layout" class:open={navIsOpen}>
+    <Navigation bind:navIsOpen />
+    <main>
         <slot />
         <footer class="page-footer">&copy; 2023 Tower of Fantasy Index.</footer>
     </main>
 </div>
-
 <BackgroundImage />
 
 <style global lang="scss">
-    .page-wrapper {
-        overflow-x: hidden;
-        margin-left: 250px;
-    }
-
-    @media (max-width: 800px) {
-        .page-wrapper {
-            margin-left: 0;
-        }
-
-        .slide .page-contents {
-            transform: translateX(250px) !important;
-        }
-    }
-
-    .page-contents {
-        background: var(--surface1);
-        max-width: 800px;
-        margin: 0 auto;
-        align-content: flex-start;
-        transition: all 0.3s ease;
-        // box-shadow: 0 0 4px 2px var(--surface-shadow);
+    .layout {
+        --content-max-width: 800px;
+        --nav-width: 250px;
         display: grid;
+        grid-template-columns: 1fr var(--content-max-width) 1fr;
+        transition: margin 0.3s ease;
+    }
+
+    @media (prefers-reduced-motion) {
+        .layout {
+            transition: unset;
+        }
+    }
+
+    // 2nav-width + content-width
+    @media (max-width: 1300px) {
+        .layout {
+            grid-template-columns: var(--nav-width) 1fr;
+        }
+    }
+
+    // content-width
+    @media (max-width: 800px) {
+        .layout {
+            grid-template-columns: var(--nav-width) 100vw;
+
+            &:not(.open) {
+                // margin instead of transform provides better framerate on firefox mobile, and allows the nav toggle button to stay fixed at the top; the stacking context gets messed up with transform with the current structure (nav-toggle inside of nav)
+                margin-left: calc(-1 * var(--nav-width));
+            }
+        }
+    }
+
+    main {
+        background: var(--surface1);
+        width: 100%;
+        max-width: var(--content-max-width);
+        margin: 0 auto;
+        display: grid;
+        align-content: flex-start;
         grid-template-columns: 1fr min(50ch, calc(100% - 2rem)) 1fr;
         column-gap: 1rem;
 
