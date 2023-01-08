3e76d19d4716099cc9023dab2e0e19a8cc4eb62d
Zakum <mirveo@gmail.com>
2023-01-08 06:33:51 -0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -12,20 +12,6 @@ const config = {
 	],
 	extensions: ['.svelte', '.md'],
 	kit: {
-		// adapter: adapter(
-		// 	{
-		// 		// default options are shown. On some platforms
-		// 		// these options are set automatically — see below
-		// 		pages: 'build',
-		// 		assets: 'build',
-		// 		fallback: null,
-		// 		precompress: false
-		// 	}
-		// ),
-		// prerender: {
-		// 	// This can be false if you're using a fallback (i.e. SPA mode)
-		// 	default: true
-		// }
 		adapter: adapter()
 	}
 };
