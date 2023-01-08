eaae04ad777035be8ffade493a0493135505db7b
Zakum <mirveo@gmail.com>
2023-01-08 02:59:21 -0800

    cloudflare pages config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex'
 
@@ -8,11 +8,24 @@ const config = {
 		preprocess(),
 		mdsvex({
 			extensions: ['.md']
-		  })
+		})
 	],
 	extensions: ['.svelte', '.md'],
 	kit: {
-		adapter: adapter()
+		adapter: adapter(
+			{
+				// default options are shown. On some platforms
+				// these options are set automatically — see below
+				pages: 'build',
+				assets: 'build',
+				fallback: null,
+				precompress: false
+			}
+		),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
