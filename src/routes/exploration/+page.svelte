567a477e8a9266695d9eb8b64b05d47937b14704
Zakum <mirveo@gmail.com>
2023-01-08 16:33:40 -0800

    #303 solved

--- src/routes/exploration/+page.svelte
+++ src/routes/exploration/+page.svelte
@@ -7,16 +7,17 @@
     import Ad from "$lib/components/Ad.svelte";
     onMount(() => {
         const anchors = new AnchorJS();
-        anchors.add("h2");
+        anchors.add("h3");
     });
 </script>
 
-<SectionNavigation />
+<SectionNavigation links={["abyss", "grayspace", "mirroria", "vera", "aesperia"]} />
 <h1>Exploration</h1>
 
 <Ad unit="lb1" />
 <Ad unit="mobile_mpu1" />
 
+<h2 id="abyss">Confounding Abyss</h2>
 <div class="table-wrapper">
     <table class="bg-alternate">
         <thead>
@@ -24,7 +25,7 @@
             <th>Solution</th>
         </thead>
         <tbody>
-            {#each puzzles as puzzle}
+            {#each puzzles.filter(p => p.location === "disorderly abyss") as puzzle}
                 <tr>
                     <td>
                         <div class="img-and-name">
@@ -35,6 +36,137 @@
                                 height="120"
                             />
                             <h2>{puzzle.name}</h2>
+                        </div>
+                    </td>
+                    <td>
+                        <SvelteMarkdown source={puzzle.solution} />
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
+
+<Ad unit="lb3" />
+<Ad unit="mobile_mpu2" />
+
+<h2 id="grayspace">Grayspace & Twilight Zone</h2>
+<div class="table-wrapper">
+    <table class="bg-alternate">
+        <thead>
+            <th>Puzzle</th>
+            <th>Solution</th>
+        </thead>
+        <tbody>
+            {#each puzzles.filter(p => p.location === "gray zone" || p.location === "dark zone") as puzzle}
+                <tr>
+                    <td>
+                        <div class="img-and-name">
+                            <img
+                                src={`/images/exploration/${puzzle.imgSrc}.webp`}
+                                alt={puzzle.name}
+                                width="150"
+                                height="120"
+                            />
+                            <h2>{puzzle.name}</h2>
+                        </div>
+                    </td>
+                    <td>
+                        <SvelteMarkdown source={puzzle.solution} />
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
+
+<Ad unit="lb4" />
+<Ad unit="mobile_lb1" />
+
+<h2 id="mirroria">Mirroria</h2>
+<div class="table-wrapper">
+    <table class="bg-alternate">
+        <thead>
+            <th>Puzzle</th>
+            <th>Solution</th>
+        </thead>
+        <tbody>
+            {#each puzzles.filter(p => p.location === "mirroria") as puzzle}
+                <tr>
+                    <td>
+                        <div class="img-and-name">
+                            <img
+                                src={`/images/exploration/${puzzle.imgSrc}.webp`}
+                                alt={puzzle.name}
+                                width="150"
+                                height="120"
+                            />
+                            <h2>{puzzle.name}</h2>
+                        </div>
+                    </td>
+                    <td>
+                        <SvelteMarkdown source={puzzle.solution} />
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
+
+<Ad unit="mobile_lb3" />
+
+<h2 id="vera">Vera</h2>
+<div class="table-wrapper">
+    <table class="bg-alternate">
+        <thead>
+            <th>Puzzle</th>
+            <th>Solution</th>
+        </thead>
+        <tbody>
+            {#each puzzles.filter(p => p.location === "vera") as puzzle}
+                <tr>
+                    <td>
+                        <div class="img-and-name">
+                            <img
+                                src={`/images/exploration/${puzzle.imgSrc}.webp`}
+                                alt={puzzle.name}
+                                width="150"
+                                height="120"
+                            />
+                            <h2>{puzzle.name}</h2>
+                        </div>
+                    </td>
+                    <td>
+                        <SvelteMarkdown source={puzzle.solution} />
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
+
+<Ad unit="lb5" />
+<Ad unit="mobile_lb4" />
+
+<h2 id="aesperia">Aesperia</h2>
+<div class="table-wrapper">
+    <table class="bg-alternate">
+        <thead>
+            <th>Puzzle</th>
+            <th>Solution</th>
+        </thead>
+        <tbody>
+            {#each puzzles.filter(p => p.location === "aesperia") as puzzle}
+                <tr>
+                    <td>
+                        <div class="img-and-name">
+                            <img
+                                src={`/images/exploration/${puzzle.imgSrc}.webp`}
+                                alt={puzzle.name}
+                                width="150"
+                                height="120"
+                            />
+                            <h3>{puzzle.name}</h3>
                         </div>
                     </td>
                     <td>
