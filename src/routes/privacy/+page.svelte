b8ea271990a3c32826be8e441df177d08f9f404c
Zakum <mirveo@gmail.com>
2023-01-08 20:11:45 -0800

    meta tags

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,6 +1,16 @@
 <svelte:head>
     <title>Privacy Policy | Tower of Fantasy Index</title>
     <meta name="description" content="Privacy Policy for the Tower of Fantasy Index.">
+    <meta property="og:title" content="Privacy Policy" />
+    <meta
+        property="og:description"
+        content="Privacy Policy for the Tower of Fantasy Index."
+    />
+    <meta
+        property="og:image"
+        content={`/images/mia.png`}
+    />
+    <meta name="theme-color" content="#377dcb" />
 </svelte:head>
 
 <div class="privacy-policy">
