9f669217136aad903c71d8ecc113ce93416f9dd2
Zakum <mirveo@gmail.com>
2023-01-22 00:50:22 -0800

    update guides

--- src/routes/guides/+page.svelte
+++ src/routes/guides/+page.svelte
@@ -5,3 +5,20 @@
 </script>
 
 <!-- <h1>{data.items[0].path}</h1> -->
+<p>
+    Many guides have been removed for being extremely outdated. External links have been added to guides maintained by others. View all the old guides here: 
+    https://toweroffantasy.netlify.app
+</p>
+
+<p>
+    Bygone phantasm rewards, armor RNG stat ranges, world boss drop rates, joint operation drop rates: 
+    https://docs.google.com/spreadsheets/d/1lRhBgB7M7xqqBGdMCvmW2Bwnu1OAvasIN6ukLTgOoCo/edit#gid=0
+</p>
+
+<p>
+    Mount piece drop rates: https://twitter.com/Sova_ToF/status/1562031690490560517
+</p>
+
+<p>
+    Pvp guide: https://docs.google.com/document/d/1eZVlm-Y9Uosyx9Fqy9m8N8IfY31XF6YdQjFxCwiFcfk/edit
+</p>
